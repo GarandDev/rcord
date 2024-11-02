df0c729a0af575b21d905390765ef414eefafa1c
GarandDev <frackzproductions@gmail.com>
2024-11-02 13:29:18 +0100

    Making it easier to create

--- examples/purchasedGamepass.server.lua
+++ examples/purchasedGamepass.server.lua
@@ -1,12 +1,16 @@
 local MarketplaceService = game:GetService("MarketplaceService")
 
 local rCord = require(script.Parent.Parent.rCord)
-local Webhook = rCord.Webhook.new('https://discord.com/api/webhooks/1302027217915871384/wqm1CY7TG4CuRgj6mlkUPDWTC7yiqaqxE4M0gRA5HPPXMs8iyR7xZmeVPdE9p48m5SDm')
+local Webhook = rCord.createWebhook('https://discord.com/api/webhooks/.../...')
 
 MarketplaceService.PromptGamePassPurchaseFinished:Connect(function(player, passId, wasPurchased)
     if wasPurchased then
-        local message = rCord.Message.new()
-        message:addEmbed(message:createEmbed():setTitle("Player bought gamepass"):setDescription("Player **" .. player.Name .. "** bought gamepass **" .. tostring(passId) .. "**!"))
+        local message = rCord.createMessage()
+        message:addEmbed(
+            rCord.createEmbed()
+                :setTitle("Player bought gamepass")
+                :setDescription("Player **" .. player.Name .. "** bought gamepass **" .. tostring(passId) .. "**!")
+        )
 
         local success, response = Webhook:send(message)
         assert(success, "Failed to send webhook :(((( response: " .. response)
